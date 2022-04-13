27166a9c7be7601e28ed520e93e4d25d118393e2
alecgreen24 <alecjoshuagreen@gmail.com>
2022-04-13 12:32:34 -0400

    Added editTest, accountInfo, and newTest pages

--- multiple_choice/views.py
+++ multiple_choice/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .dao import connect
-
 
 
 # Create your views here.
 def index(request):
-    connect()
-    return render(request, 'login.html')
+    return render(request, 'newTest.html')
