daf2431eb882c2d2f81113d670f0608f413cf875
Kyler England <kylerengland32@gmail.com>
2022-04-19 22:16:14 -0400

    Added select to dai

--- multiple_choice/daos/test_dao.py
+++ multiple_choice/daos/test_dao.py
@@ -51,6 +51,45 @@ class TestDAO():
             # Returns the status of the commit if the change was successful.
             return "success"
 
+    
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            return error
+        finally:
+            if conn is not None:
+                conn.close()
+
+def selectTest(self, test):
+        sql = f"""SELECT q.id as question_id, 
+                q.content as question_content,  
+                qa.answer_id, 
+                answer.content as answer_content 
+                FROM question q
+                JOIN question_answer qa
+                ON q.id = qa.question_id
+                JOIN answer
+                ON qa.answer_id =  answer.id
+                WHERE q.id IN (SELECT question_id
+                FROM test_question
+                WHERE test_id = '{test.id}');"""
+        conn = None
+        try:
+            # Establishing the connection
+            conn = psycopg2.connect(host=self.host, database=self.database, user=self.user, password=self.password)
+            # Create a cursor
+            cur = conn.cursor()
+            # Execute a statement.
+            cur.execute(sql)
+            # Fetch the id of the test created.
+            id = cur.fetchall()
+            # Commit the changes on the table.
+            conn.commit()
+            # Close the communication with the PostgreSQL
+            cur.close()
+            # Returns the status of the commit if the change was successful.
+            return id[0]
+
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
             return error
