92326c409fd4cb1b4685d0c460ffdcb04d4bb2d0
Mariano Jalif <marigyt.maj@gmail.com>
2022-04-20 00:49:16 -0400

    Commiting to Mariano in order to use Kyler's changes in his branch.

--- multiple_choice/database_schema/queries.sql
+++ multiple_choice/database_schema/queries.sql
@@ -1,6 +1,14 @@
--- Get the question content and correct answer content
-SELECT question.content, answer.content FROM
-question, question_answer, answer
-WHERE question.correct_answer_id = answer.id
-AND question.id = question_answer.question_id
-AND answer.id = question_answer.answer_id 
+-- Get the questions and thier answers for one test.
+SELECT 
+q.id as question_id, 
+q.content as question_content, 
+qa.answer_id, 
+answer.content as answer_content
+FROM question q
+JOIN question_answer qa
+ON q.id = qa.question_id
+JOIN answer
+ON qa.answer_id =  answer.id
+WHERE q.id IN (SELECT question_id
+FROM test_question
+WHERE test_id = 62)
